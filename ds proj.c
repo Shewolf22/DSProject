63819811400f4914d8898b0dafcedc307a456363
snehawadekar <43239574+snehawadekar@users.noreply.github.com>
2018-09-15 15:57:36 +0530

    Add files via upload
    sampada plz download this

--- ds proj.c
+++ ds proj.c
@@ -1,44 +1,23 @@
 #include <stdio.h>
-
 #include <conio.h>
-
 #include <string.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 #define finerate 5
-
 #define MAX 100
-
-
-
 FILE *fp;
 
-
-
 typedef struct book{
-
     char title[MAX];
-
     char author[MAX];
-
     char ID[MAX];
-
     //insert the issue record linked list node here later lmao
-
 }Book;
-
-
-
 //This is a temporary linked list to store the data read from the data.txt file because idk shit about trees
 
 typedef struct node{
-
     Book book;
-
     struct node *next;
-
 }node;
 
 node *START = NULL;
@@ -48,132 +27,67 @@ typedef struct dnode   // a doubly linked list
            int issuedate;
            struct dnode *next,*prev;
         }dnode;
-
-
 dnode *issue_record(char bi,int issda,dnode *head);
-
 int ntbe(int bi,dnode *head);
-
 char* currtime();
-
 float finecalc(int,int);
-
 int datecalc();
-
 int find_issue_date(dnode *head,int bi);
-
-
 node *create(Book, node*);
-
 void insert(Book);
-
 void replaceSpaces(char *);
-
 void replaceUnder(char *);
-
 void display();
-
 void getBookData(Book *);
-
 void writeToFile(Book *);
-
 void readFromFile();
-
-
-
 int main()
-
 {
-    int isd,cd;char bi;float fineamount;
-
+    int isd,cd;char bi[MAX];float fineamount;
     node *head;
-
     Book new;
-
     char c = 'y';
-
     int sw;
     do
     {
-
     printf("Yay I made something lmao\n");
-
     printf("1.Add Book\n2.Display Books\n3.Issue Books\n4.return books\n");
-
     scanf("%d", &sw);
-
-
-
     switch(sw)
-
     {
-
-        case 1:
-
-
-
-                    printf("Enter the details yo\n");
-
+        case 1:     printf("Enter the details yo\n");
                     getBookData(&new);
-
                     writeToFile(&new);
-
                     printf("Do you want to continue? [y\\n]");
-
                     scanf("%c", &c);
-
                     //clrscr();
-
-
-
                 break;
-
         case 2: readFromFile();
-
                 display();
-
                 break;
-
-        case 3:printf("enter the book id");
+        case 3: printf("enter the book id");
                 gets(bi);
                 isd=datecalc();
                 head=issue_record(bi,isd,head);
                 break;
-
-        case 4:printf("enter the bookid");
+        case 4: printf("enter the bookid");
                 gets(bi);
                 isd=find_issue_date(bi,head);
                 cd=datecalc;
                 if(isd==-1)
                 {
                     printf("book not found\n");
-
                 }
-
                 fineamount=finecalc(isd,cd);
-
                 printf("fine to be paid:%f",fineamount);
-
-
-
-
-
-
         default: printf("Wrong choice m8\n");
-
     }
     }while(c != 'n');
-
-
-
     return 0;
-
 }
-
 char* currtime()                              //fn gives current day_date_time_year
 {
    time_t ct;
-
    ct=time(NULL);
    char* cts;
    cts=ctime(&ct);
@@ -221,7 +135,7 @@ int find_issue_date(dnode *head,int bi)
  dnode *issue_record(char bi,int issda,dnode *head)
     {
         dnode *p,*q;
-        p=(dnode *)malloc(sizeof(dnode));
+        p=(dnode*)malloc(sizeof(dnode));
         p->bookid=bi;
         p->issuedate=issda;
         if(head==NULL)
@@ -264,324 +178,156 @@ int ntbe(int nbi,dnode *head)    //THIS FN CALCULATES THE NUMBER OF TIMES A BOOK
     }
     return count;
 }
-
-
-
-
-
-
-
-
 //Gets the book details from user
-
 void getBookData(Book *new_book)
-
 {
-
     fflush(stdin); //Flush the stdin so that no garbage value gets picked up
-
-
-
     printf("Enter the book title\n");
-
     fgets(new_book->title, MAX, stdin);
-
     new_book->title[strcspn(new_book->title, "\n")] = 0; //Replaces the \n character at the end with \0
-
     replaceSpaces(new_book->title);
-
-
-
     printf("Enter the author\n");
-
     fgets(new_book->author, MAX, stdin);
-
     new_book->author[strcspn(new_book->author, "\n")] = 0;
-
     replaceSpaces(new_book->author);
-
-
-
     printf("Enter the book ID\n");
-
     fgets(new_book->ID, MAX, stdin);
-
     new_book->ID[strcspn(new_book->ID, "\n")] = 0;
-
 }
-
-
-
 //Writes the book details to data.txt
-
 void writeToFile(Book *b)
-
 {
-
     fp = fopen("data.txt", "a");
-
     if(fp == NULL)
-
     {
-
         printf("Error\n");
-
     }
-
     else
-
     {
-
         fprintf(fp, "%s|%s|%s\n", b->title, b->author, b->ID);
-
     }
-
     fclose(fp);
-
 }
-
-
 
 //Reads the books from data.txt and creates a linked list to store them
-
 void readFromFile()
-
 {
-
     fp = fopen("data.txt", "r");
-
     if(fp == NULL)
-
     {
-
         printf("Error\n");
-
     }
-
     else
-
     {
-
         char temp[MAX];
-
         while(fscanf(fp, "%s\n", temp) != EOF)
-
         {
-
             Book b;
-
             int i = 0, j = 0, k = 0;
-
             replaceUnder(temp);
 
-
-
             //Extract the Book title
-
             while(temp[i] != '|')
-
             {
-
                 b.title[i] = temp[i];
-
                 i++;
-
             }
-
             b.title[i] = '\0';
 
-
-
             i++;
-
             ///Extracts the Book Author
-
             while(temp[i] != '|')
-
             {
-
                 b.author[j] = temp[i];
-
                 i++;
-
                 j++;
-
             }
-
             b.author[j] = '\0';
 
-
-
             i++;
-
             //Extracts the Book ID
-
             while(temp[i] != '\0')
-
             {
-
                 b.ID[k] = temp[i];
-
                 i++;
-
                 k++;
-
             }
-
             b.ID[k] = '\0';
-
             insert(b);
-
         }
-
     }
-
     fclose(fp);
-
 }
-
-
 
 node* create(Book b,node* next)
-
 {
-
     node* new_node = (node*)malloc(sizeof(node));
-
     if(new_node == NULL)
-
     {
-
         printf("Error creating a new node.\n");
-
         exit(0);
-
     }
-
     new_node->book = b;
-
     new_node->next = next;
 
-
-
     return new_node;
-
 }
-
-
 
 void insert(Book b)
-
 {
-
     node *newnode, *cursor;
 
-
-
     if(START == NULL)
-
     {
-
         newnode = create(b, NULL);
-
         START = newnode;
-
     }
-
     else
-
     {
-
         cursor = START;
-
         while(cursor->next != NULL)
-
         {
-
             cursor = cursor->next;
-
         }
-
         newnode = create(b, NULL);
-
         cursor->next = newnode;
-
     }
-
 }
-
-
 
 void display()
-
 {
-
     node *cursor;
-
     cursor = START;
-
     while(cursor != NULL)
-
     {
-
         printf("Book Title: %s\t\t", cursor->book.title);
-
         printf("Book Author: %s\t\t", cursor->book.author);
-
         printf("Book ID: %s\n", cursor->book.ID);
-
         cursor = cursor->next;
-
     }
-
 }
-
-
 
 void replaceSpaces(char *c)
-
 {
-
     int i;
-
     for(i = 0; i < strlen(c); i++)
-
     {
-
         if(c[i] == ' ')
-
         {
-
             c[i] = '_';
-
         }
-
     }
-
 }
-
-
 
 void replaceUnder(char *c)
-
 {
-
     int i;
-
     for(i = 0; i < strlen(c); i++)
-
     {
-
         if(c[i] == '_')
-
         {
-
             c[i] = ' ';
-
         }
-
     }
-
 }
-
 
